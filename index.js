bcce8a6cfa1931497943c4d0da065fc3a915aee0
alenorgue <alenoriegag@gmail.com>
2025-03-31 20:33:45 +0200

    finished

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // ¡Buena suerte!
+const btnCalTotal = document.getElementById('calculate');
+const billInput = document.getElementById('bill');
+const tipInput = document.getElementById('tip');
+const outputTotal = document.getElementById('total');
+
+function calculateTotal(){
+    const billValue = billInput.value;
+    const tipValue = tipInput.value;
+    const totalValue = billValue * (1 + tipValue / 100);
+    outputTotal.innerHTML = totalValue.toFixed(2);
+}
+
+btnCalTotal.addEventListener('click', calculateTotal)
